1e817afb5439c927a5d77b5b42bf8660a4358404
Alexey <alesha.kolpashikov@gmail.com>
2021-09-14 21:26:56 +0300

    Add tests for StringExtension

--- AdditionalСhecks/StringExtension.cs
+++ AdditionalСhecks/StringExtension.cs
@@ -1,8 +1,47 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace AdditionalСhecks
 {
-    public class Class1
+    static public class StringExtension
     {
+        /// <summary>
+        /// Get time from string in format hh:mm.
+        /// </summary>
+        /// <param name="str">Input string</param>
+        /// <returns>Time in format hh:mm</returns>
+        public static string GetTime(this string str)
+        {
+            Regex rg = new Regex(@"([0-1]\d|2[0-3]):([0-5]\d");
+            var result = rg.Match(str);
+            return result.Value;
+        }
+        /// <summary>
+        /// Checks if the entered string is a Mac address.
+        /// </summary>
+        /// <param name="str">Input string</param>
+        /// <returns></returns>
+        public static bool IsMacAdress(this string str)
+        {
+            return Regex.IsMatch(str, @"(([0 - 9a - fA - F]{ 2}):){ 5} ([0 - 9a - fA - F]{ 2})");
+        }
+        /// <summary>
+        /// Checks if the entered string is an email address.
+        /// </summary>
+        /// <param name="str">Input string</param>
+        /// <returns></returns>
+        public static bool IsEmailAdress(this string str)
+        {
+            return Regex.IsMatch(str, @"([\w] +)@([\w]{ 1,63}\.?[\w]{ 1,63})");
+        }
+        /// <summary>
+        /// Checks if the entered password is strong.
+        /// </summary>
+        /// <param name="str">Input string</param>
+        /// <returns></returns>
+        public static bool IsStrongPassword(this string str)
+        {
+            return Regex.IsMatch(str, @"(?=.*?[A - Z])(?=.*?[a - z])(?=.*?[0 - 9])[\w -]{ 8,}");
+        }
     }
 }
